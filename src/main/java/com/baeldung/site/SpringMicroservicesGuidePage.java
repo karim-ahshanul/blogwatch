9820e43029d24911a6e3d8bee322d203a2c3c822
asif <asifanwar451@gmail.com>
2021-06-03 09:50:36 +0500

    properties renamed (JAVA-5838)

--- src/main/java/com/baeldung/site/SpringMicroservicesGuidePage.java
+++ src/main/java/com/baeldung/site/SpringMicroservicesGuidePage.java
@@ -26,6 +26,7 @@ public class SpringMicroservicesGuidePage extends BlogBaseDriver {
     }
 
     @Override    
+    @Value("${spring.microservices.page.url}")
     protected void setUrl(String pageURL) {
         this.url = this.getBaseURL() + springMicroserviceGuildeUrl;
     }
