7dc4beaa4480b2952ae5646790bf0901410a517f
asif-anwar <asifanwar451@gmail.com>
2021-06-13 10:55:04 +0500

    small changes

--- src/main/java/com/baeldung/site/SpringMicroservicesGuidePage.java
+++ src/main/java/com/baeldung/site/SpringMicroservicesGuidePage.java
@@ -3,8 +3,9 @@ package com.baeldung.site;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -12,15 +13,15 @@ import com.baeldung.common.Utils;
 
 @Component
 public class SpringMicroservicesGuidePage extends BlogBaseDriver {
-    
+
     private final String springMicroserviceGuildeUrl = "/spring-microservices-guide/";
 
-    public void clickAccessTheGuideButton() { 
-        try {
-        this.getWebDriver().findElement(By.xpath("//a//span[contains(text(), 'ACCESS THIS GUIDE')]")).click();
-        }catch(ElementNotVisibleException e ) {
-            this.acceptCookie();
-        }
+    public void clickAccessTheGuideButton() {
+
+        WebDriverWait wait = new WebDriverWait(this.getWebDriver(), 30);
+        WebElement button = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a//span[contains(text(), 'ACCESS THIS GUIDE')]")));
+        button.click();
+
     }
 
     public List<WebElement> findImages() {
@@ -28,7 +29,7 @@ public class SpringMicroservicesGuidePage extends BlogBaseDriver {
         return this.getWebDriver().findElements(By.xpath("//*[@id='tve_editor']//img"));
     }
 
-    @Override    
+    @Override
     @Value("${spring.microservices.page.url}")
     protected void setUrl(String pageURL) {
         this.url = this.getBaseURL() + springMicroserviceGuildeUrl;
