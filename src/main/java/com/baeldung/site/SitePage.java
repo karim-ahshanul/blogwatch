1f8686f37def8b1cddcc07794a8aefa70c006d38
asif-anwar <asifanwar451@gmail.com>
2021-06-23 10:13:27 +0500

    for JAVA-5996

--- src/main/java/com/baeldung/site/SitePage.java
+++ src/main/java/com/baeldung/site/SitePage.java
@@ -193,7 +193,7 @@ public class SitePage extends BlogBaseDriver {
 
     public boolean metaDescriptionTagsAvailable() {
 
-        List<WebElement> metaElements = this.getWebDriver().findElements(By.xpath("//meta[@name = 'description' or @property = 'og:description' or @property = 'twitter:description']"));
+        List<WebElement> metaElements = this.getWebDriver().findElements(By.xpath("//meta[@name = 'description' or @property = 'og:description' or @name = 'og:description' or @property = 'twitter:description' or @name = 'twitter:description']"));
 
         String metaDescription = null;
         String metaOgDescription = null;
