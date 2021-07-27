83bdec5a79b82d6e44434aea7c99c9374c400fb4
asif-anwar <asifanwar451@gmail.com>
2021-07-27 17:26:26 +0500

    Ignoring module if REAMDME throws 404.

--- src/main/java/com/baeldung/jsoup/ModuleArticleUrlsExtractor.java
+++ src/main/java/com/baeldung/jsoup/ModuleArticleUrlsExtractor.java
@@ -2,6 +2,8 @@ package com.baeldung.jsoup;
 
 import com.baeldung.common.ConsoleColors;
 import com.google.common.annotations.VisibleForTesting;
+
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -13,6 +15,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Collection;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -30,6 +33,8 @@ public class ModuleArticleUrlsExtractor {
         return moduleUrls.stream()
           .map(this::readmeUrl)
           .map(this::parseDocument)
+          .filter(Optional::isPresent)
+          .map(document -> document.get())
           .flatMap(this::selectArticleLinks)
           .map(this::toUrl)
           .collect(Collectors.toSet());
@@ -39,18 +44,24 @@ public class ModuleArticleUrlsExtractor {
         String readmeFileUrl = url.toString() + "/" + README_FILE_NAME;
         try {
             return new URL(readmeFileUrl);
-        } catch (MalformedURLException e) {
-            
+        } catch (MalformedURLException e) {            
             throw new IllegalStateException("The README file URL isn't right: " + readmeFileUrl);
         }
     }
 
     @VisibleForTesting
-    Document parseDocument(URL url) {
+    Optional<Document> parseDocument(URL url) {
         try {
-            return Jsoup.parse(url, TIMEOUT);
-        } catch (IOException e) {
-            logger.error(ConsoleColors.redBoldMessage("Error while parsing README for:{} "), url);
+            return Optional.of(Jsoup.parse(url, TIMEOUT));
+        }
+        catch(HttpStatusException httpStatusException){
+            logger.error(ConsoleColors.redBoldMessage("Error while fetching README for:{} "), url);
+            if(httpStatusException.getStatusCode() == 404) {               
+                return Optional.empty();
+            }
+            throw new IllegalStateException("A problem occurred while parsing HTML document at URL " + url, httpStatusException);
+        }
+        catch (IOException e) {           
             throw new IllegalStateException("A problem occurred while parsing HTML document at URL " + url, e);
         }
     }
